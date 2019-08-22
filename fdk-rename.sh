2538dd3fec6d541a30971e776ff51767948ab1a6
Gjermund Skobba <gjermund@skobba.net>
2019-08-22 21:23:54 +0200

    Fixed static filetype from "*.jsx" to $filetype.

--- fdk-rename.sh
+++ fdk-rename.sh
@@ -2,7 +2,7 @@
 
 path=~/github_brreg/fdk/applications
 replace_list=./fdk-rename-list.txt
-file_type=*.scss
+file_type="*.jsx"
 
 IFS=$'\n'       # make newlines the only separator
 set -f          # disable globbing
@@ -11,5 +11,5 @@ for i in $(cat < "$replace_list"); do
     IFS=' ' read -ra VALUES <<< "$i"    #Convert string to array
     #echo Old: ${VALUES[0]} - New: ${VALUES[1]}
 
-    ./search-and-replace-in-files.sh $path "*.jsx" ${VALUES[0]} ${VALUES[1]}
+    ./search-and-replace-in-files.sh $path $file_type ${VALUES[0]} ${VALUES[1]}
 done
